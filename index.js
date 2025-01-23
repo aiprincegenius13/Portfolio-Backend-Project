9cd1806b32146db0ff93e86b3cdf49c91741d689
aiprincegenius13 <ejblack369@icloud.com>
2025-01-23 13:52:53 -0800

    html form addedmore routes added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3000; 
+const port = 8081; 
 
-// app.get("/" (req, res) => {
-//     res.send("My portfolio root path")
-// });
 
+app.use("/", (req, res) => {
+    res.send("My portfolio root path");
+});
 
 app.listen(port, () => {
     console.log("Server for Portfolio Backend is running")
 });
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
---
+++ routes.js
@@ -0,0 +1,31 @@
+var require = nodeRequire
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+res.render("index", {
+    name: "Jamar Johnson",
+    bio: "Short Bio"
+    });
+});
+
+app.get("/api/about", (req, res) => {
+    res.json({
+        name:"Your Jamar JOhnson",
+        bio:"This is about me",
+        Skills: ["skill1", "skill2", "skill3"]
+    }
+});
+
+app.get("/api/projects", (req, res) => {
+    res.json([
+       {id: 1, title: "Portfolio Website", description: "My personal website"},
+       {id: 2, title: "project1", description: "A description of project"},
+    ])
+})
+
+app.post("/api/contact", (req, res) => {
+    const {name, message} = req.body;
+    console.log(`Message from ${name}: ${message}`);
+    res.send("Thank you for your message");
+})
