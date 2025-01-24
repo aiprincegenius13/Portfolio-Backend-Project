bfebf4ebbc05b4024809589a843b6d360e4c43ec
aiprincegenius13 <ejblack369@icloud.com>
2025-01-24 15:43:36 -0800

    added views folder and index.ejs file, other editing

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const routes = require("./routes"); // Assuming your route file is named 'route.js'
 const errorHandler = require("./error"); // Import the error handling middleware
 const app = express();
-const port = 8081; 
+const port = 8085; 
+const path = require("path");
 
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
@@ -10,13 +11,24 @@ app.use(express.urlencoded({ extended: true }));
 
 // Logger middleware
 app.use((req, res, next) => {
+    const now = new Date().toISOString();
     console.log(`${req.method} ${req.url}`);
     next();
 });
 
 // View engine setup
 app.set("view engine", "ejs");
-app.set("views", "./views");
+// app.set("views", "./views");
+
+app.set("views", path.join(__dirname, "views"));
+
+
+//Route to render a view
+// app.get("/", (req, res) => {
+//     res.render("index");
+// })
+
+
 
 // Routes
 app.use("/", routes);
